2876c9e0e01c54bd242b094d00214e70b2b875e4
Ashley Bishop <cumminsgrl87@gmail.com>
2021-10-26 11:13:51 -0400

    JS file update

--- dayTrip.js
+++ dayTrip.js
@@ -1,2 +1,51 @@
 //dayTrip
 
+"use strict"
+
+function trips() {
+    
+let Destination = ["Dallas", "New Orleans", "Nashville", "Miami", "Cincinatti"];
+let Resturaunt = ["Outback Steakhouse", "Applebees", "Olive Garden", "Cheesecake Factory", "Buffalo Wild Wings"];
+let Transportation = ["Trolly", "Electric Bike", "Uber", "Rental Car", "Limo"];
+let Entertainment = ["Live Music", "Grand Ole Opry", "Beer Garden", "Rodeo", "Fishing"];
+
+console.log(Destination);
+console.log(Resturaunt);
+console.log(Transportation);
+console.log(Entertainment);
+
+let [rand_destination = Math.floor(Math.random()*(Destination.length [5]),
+let [rand_resturaunt = Math.floor(Math.random()*(Resturaunt.length [5]),
+let [rand_transportation = Math.floor(Math.random()*(Transportation.length [5]),
+let [rand_entertainment = Math.floor(Math.random()*(Entertainment.length [5]),
+
+let ,first = document.getElementById('Destination'))
+[let ,second = document.getElementById('Resturaunt')]
+[let [third] = document.getElementById('Transportation'),
+let ,fourth = document.getElementById('Entertainment')]
+
+[first.value = Destination[rand_first]
+[second.value = Resturaunt[rand_second]
+[third.value = Transportation[rand_third]
+[fourth.value = Entertainment[rand_fourth]
+
+[console.log(first)
+[console.log(second)
+[console.log(third)
+[console.log(fourth)]
+
+
+[prompt("Welcome to Day Trip Generator! Do you like this trip?")]
+[prompt (trips)]
+[prompt (first, second, third, fourth)
+
+
+]
+
+[prompt("Type Yes or No")]
+[trips ("Destination", "Resturaunt", "Transportation", "Entertainment")
+[trips = (userInput, trips)]
+
+]
+
+}
